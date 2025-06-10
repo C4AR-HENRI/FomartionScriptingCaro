dc069bf210c910400fe46722b2c1555fe3edcb46
C4AR-HENRI <carolina4aleixo@gmail.com>
2025-06-10 09:27:40 +0200

    Update my_script.js

--- my_script.js
+++ my_script.js
@@ -1,6 +1,16 @@
 function my_func(){
- MessageBox.information("au revoir")
 
-//fait sur github
- 
+    //declaration de lavariable myvar et assignation d'une valeur de 1
+
+    var my_var = 2
+    
+    MessageBox.information(my_var)
+    
+    var my_var_2 = 10
+
+    MessageBox.information(my_var_2)
+
+    var my_var_3 = my_var + my_var_2
+
+    MessageLog.trace(my_var_3)
 }
